6bea8a2759254af4ea2b1d970117bc775b230e1b
victoriataracenko <victoriataracenko0412@gmail.com>
2025-03-10 16:00:42 +0200

    done 1-2 tasks

--- hw-11/js/task01.js
+++ hw-11/js/task01.js
@@ -1,518 +1,92 @@
 "use strict"
 
-let arr = [35, 0, 250, -5, 69, -250, -86, 444, 0, 1, -58]
-// const winArr = arr.filter((el)=> el>0)
-// document.write(winArr)
-
-// arr.forEach((el)=> {
-//     if(el>0) document.write(`${el} <br>`)
-// })
-
-// arr.forEach((el, ind, arr)=> {
-//     if(el>0) arr[ind] *=0.8
-// })
-// document.write(`${arr} <br>`)
-
-// const dolArr = arr.map((el)=> el/40)
-// document.write(`${ dolArr} <br>`)
-
-// const sum = arr.reduce((prevSum, el) => prevSum+el)
-// document.write(`${ sum} <br>`)
-
-// const sum = arr.reduce((prevSum, el) => el>0 ? prevSum+el : prevSum, 0)
-// document.write(`${ sum} <br>`)
-
-// const sum = arr.reduce((prevSum, el) => el<0 ? prevSum+el : prevSum, 0)
-// document.write(`${ sum} <br>`)
-
-// // ----1
-// console.log(arr.includes(0))
-// // ----2
-// console.log(arr.some(el=> el===0))
-
-// const numZero = arr.reduce((prevSum, el) => el===0 ? prevSum+1 : prevSum, 0)
-// document.write(`${ numZero} <br>`)
-
-// const numMore100 = arr.reduce((prevSum, el) => el > 100 ? prevSum+1 : prevSum, 0)
-// document.write(`${numMore100} <br>`)
-
-// const arr2 = arr.slice(arr.indexOf(0)+1)
-// document.write(`${arr2} <br>`)
-
-// const arr2 = arr.slice(arr.lastIndexOf(0)+1)
-// document.write(`${arr2} <br>`)
-
-// arr = arr.filter(el => el >=20)
-// document.write(`${arr} <br>`)
-
-// arr = arr.filter(el => el !==0)
-// document.write(`${arr} <br>`)
-
-// arr.splice(arr.includes(0), 1)
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> a-b)
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> b-a)
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> {
-//     let result
-//     if (a<0 && b>=0) result = -1
-//     else if (a>=0 && b<0) result = 1
-//     else result = 0 
-
-//     return result
-// })
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> {
-//     let result
-//     if (a<0 && b>=0) result = -1
-//     else if (a>=0 && b<0) result = 1
-//     else result = a<0 ? 0 : a- b 
-
-//     return result
-// })
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> {
-//     let result
-//     if (a<0 && b>=0) result = -1
-//     else if (a>=0 && b<0) result = 1
-//     else result = a >= 0 ? 0 : a - b
-
-//     return result
-// })
-// document.write(`${arr} <br>`)
-
-
-// arr.sort((a,b)=> {
-//     let result
-//     if (a===0 && b!==0) result = -1
-//     else if (a!==0 && b===0) result = 1
-//     else result = a- b 
-
-//     return result
-// })
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> {
-//     let result
-//     if (a===0 && b!==0) result = -1
-//     else if (a!==0 && b===0) result = 1
-//     else result = b - a 
-
-//     return result
-// })
-// document.write(`${arr} <br>`)
-
-// arr.sort((a,b)=> {
-//     let result
-//     if (a > 200 && b<=200) result = -1
-//     else if (a<=200 && b>200) result = 1
-//     else result = 0
-
-//     return result
-// })
-// document.write(`${arr} <br>`)
-
-let incomes = [
-    // 0  1  2  3  4  5  6
-    [1, 2, 5, 2, 1, 2, 9], // 0
-    [1, 2, 1, 2, 1, 2, 9], // 1
-    [1, 1, 5, 2, 1, 2, 9], // 2
-    [1, 6, 5, 2, 1, 2, 9], // 3
-    [1, 2, 23, 2, 1, 2, 9], // 4
-    [0, 1, 5, 2, 1, 2, 9], // 5
-    [1, 1, 54, 21, 1, 2, 9], // 6
-    [14, 11, 54, 21, 1, 2, 9], // 7
+const arr = [
+//   0  1   2  3
+    [1, 2, 3, 4], //0
+    [5, 6, 7, 8],//1
+    [1, 2, 1, 4], //2
+    [5, 1, 7, 1],//3
 ]
-//  ---1
-// let sum = 0
-// for (let row of incomes) {
-//     for (let el of row) {
-//         sum += el
-//     }
-// }
 
-//---2
-// for (let rowIndex = 0; rowIndex < incomes.length; rowIndex++) {
-//     for (let colIndex = 0; colIndex < incomes[rowIndex].length; colIndex++) {
-//         sum += incomes[rowIndex][colIndex]
-//     }
-// }
-// document.write(sum)
+const middleRow = Math.floor(arr.length / 2)
+const rowLength = arr.length
 
-// let sum = 0
-// for (let rowIndex = 0; rowIndex < incomes.length; rowIndex++) {
-//     for (let colIndex = 0; colIndex < 5; colIndex++) {
-//         sum += incomes[rowIndex][colIndex]
-//     }
-// }
-// document.write(sum)
-
-// const profits = [
-//     [1200, 1500, 1700, 1600], // Прибуток за перший місяць
-//     [1400, 1600, 1800, 1700], // Прибуток за другий місяць
-//     [1300, 1550, 1750, 1650], // Прибуток за третій місяць
-//     [1100, 1450, 1600, 1550], // Прибуток за четвертий місяць
-//     [1250, 1500, 1850, 1800], // Прибуток за п’ятий місяць
-//     [1350, 1600, 1900, 1700], // Прибуток за шостий місяць
-//     [1450, 1700, 2000, 1800], // Прибуток за сьомий місяць
-//     [1500, 1750, 2100, 1900], // Прибуток за восьмий місяць
-//     [1550, 1800, 2200, 2000], // Прибуток за дев’ятий місяць
-//     [1600, 1850, 2300, 2100], // Прибуток за десятий місяць
-//     [1650, 1900, 2400, 2200], // Прибуток за одинадцятий місяць
-//     [1700, 2000, 2500, 2300], // Прибуток за дванадцятий місяць
-// ]
-
-// let numVisitors = 0
-// ---1
-// for(let row of profits) {
-//     for (let el of row) {
-//         numVisitors += el
-//     }
-// }
-// ----2
-// for (let i = 0; i < profits.length; i++) {
-//     for (let j = 0; j < profits[i].length; j++) {
-//         numVisitors += profits[i][j]
-//     }
-// }
-// document.write(numVisitors)
-
-// let numVisitorsSummer = 0
-// for (let i = 5; i <= 7; i++) {
-//     for (let j = 0; j < profits[i].length; j++) {
-//         numVisitorsSummer += profits[i][j]
-//     }
-// }
-// document.write(numVisitorsSummer)
-
-// let numVisitors = 0
-// for (let i = 6; i < profits.length; i++) {
-//     for (let j = 0; j < profits[i].length; j++) {
-//         numVisitors  += profits[i][j]
-//     }
-// }
-// document.write(numVisitors )
-
-// let usersHeignts = [
-//     [167, 176, 190, 171, 167], //0
-//     [167, 176, 167], //1
-//     [167, 176, 190, 171, 167, 145, 134], //2    usersHeignts[2]
-//     [167, 176], //3
-// ]
-
-// let sum = 0
-// ---1
-// for (let i = 0; i < usersHeignts.length; i++) {
-//     for (let j = 0; j < usersHeignts[i].length; j++) {
-//         sum += usersHeignts[i][j]
-//     }
-// }
-// ---2
-// for (let row of usersHeignts) {
-//     for (let el of row) {
-//         sum +=el
-//     }
-
-// }
-// document.write(sum)
-
-// let max = -Infinity
-
-// for (let row of usersHeignts) {
-//     for (let el of row) {
-//         if(el > max) max=el
-//     }
-// }
-// document.write(max)
-
-
-
-// for (let row of usersHeignts) {
-//     let max = -Infinity
-//     for (let el of row) {
-//         if(el > max) max=el
-//     }
-//     document.write(`${max}<br>`)
-// }
-
-// let sum = 0
-// let num = 0
-
-// for (let i = 0; i < usersHeignts.length; i++) {
-//     for (let j = 0; j < usersHeignts[i].length; j++) {
-//         sum += usersHeignts[i][j]
-//     }
-//     num += usersHeignts[i].length
-// }
-// console.log (num)
-// document.write(`${sum/num}`)
-
-
-// for (let i = 0; i < usersHeignts.length; i++) {
-//     let sum = 0
-//     let num = 0
-//     for (let j = 0; j < usersHeignts[i].length; j++) {
-//         sum += usersHeignts[i][j]
-//     }
-//     num += usersHeignts[i].length
-//     document.write(`${sum/num} <br>`)
-// }
-
-// const schoolsData = [
-//     [
-//         // Перший клас першої школи
-//         [160, 165, 170, 150, 145, 180, 175, 155, 140, 165, 170],
-//         // Другий клас першої школи
-//         [
-//             150, 160, 170, 180, 190, 150, 160, 165, 170, 175, 180, 185, 190,
-//             145, 150,
-//         ],
-//         // Третій клас першої школи
-//         [145, 150, 155, 160, 165, 170, 175, 180, 140, 145, 150],
-//         // Четвертий клас першої школи
-//         [
-//             165, 170, 175, 160, 165, 170, 175, 180, 185, 190, 140, 145, 150,
-//             155, 160,
-//         ],
-//         // П'ятий клас першої школи
-//         [140, 145, 150, 155, 160, 165, 170, 175, 140, 145, 150],
-//         // Шостий клас першої школи
-//         [160, 165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160, 165],
-//         // Сьомий клас першої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 190, 140, 145, 150],
-//         // Восьмий клас першої школи
-//         [145, 150, 155, 160, 165, 170, 175, 140, 145, 150, 155, 160],
-//         // Дев'ятий клас першої школи
-//         [
-//             165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160, 165, 170,
-//             175, 180,
-//         ],
-//         // Десятий клас першої школи
-//         [140, 145, 150, 155, 160, 165, 170, 175, 140, 145],
-//         // Одинадцятий клас першої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150, 155],
-//         ],
-//         [
-//         // Перший клас другої школи
-//         [150, 155, 160, 165, 170, 175, 180, 145, 150, 155, 160, 165],
-//         // Другий клас другої школи
-//         [140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 140, 145],
-//         // Третій клас другої школи
-//         [
-//             160, 165, 170, 175, 180, 185, 190, 150, 160, 165, 170, 175, 180,
-//             185,
-//         ],
-//         // Четвертий клас другої школи
-//         [155, 160, 165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160],
-//         // П'ятий клас другої школи
-//         [140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150],
-//         // Шостий клас другої школи
-//         [
-//             160, 165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160, 165,
-//             170, 175,
-//         ],
-//         // Сьомий клас другої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150],
-//         // Восьмий клас другої школи
-//         [145, 150, 155, 160, 165, 170, 175, 140, 145, 150],
-//         // Дев'ятий клас другої школи
-//         [165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160, 165],
-//         // Десятий клас другої школи
-//         [140, 145, 150, 155, 160, 165, 170, 175, 140, 145],
-//         // Одинадцятий клас другої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150, 155],
-//         ],
-//         [
-//         // Перший клас третьої школи
-//         [160, 165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160],
-//         // Другий клас третьої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 140, 145, 150],
-//         // Третій клас третьої школи
-//         [
-//             140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150,
-//             155, 160,
-//         ],
-//         // Четвертий клас третьої школи
-//         [165, 170, 175, 180, 185, 190, 140, 145, 150, 155, 160, 165, 170],
-//         // П'ятий клас третьої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150, 155],
-//         // Шостий клас третьої школи
-//         [160, 165, 170, 175, 180, 185, 190, 150, 155, 160, 165],
-//         // Сьомий клас третьої школи
-//         [140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 140, 145],
-//         // Восьмий клас третьої школи
-//         [155, 160, 165, 170, 175, 180, 185, 190, 150, 155, 160, 165],
-//         // Дев'ятий клас третьої школи
-//         [
-//             140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 145, 150,
-//             155,
-//         ],
-//         // Десятий клас третьої школи
-//         [150, 155, 160, 165, 170, 175, 180, 185, 140, 145, 150],
-//         // Одинадцятий клас третьої школи
-//         [
-//             160, 165, 170, 175, 180, 185, 190, 150, 155, 160, 165, 170, 175,
-//             180,
-//         ],
-//     ],
-// ]
-
-// let max = -Infinity
-// ----1
-// for (let scholl of schoolsData) {
-//     for (let classList of scholl) {
-//         for (let pupilHeight of classList) {
-//             if(pupilHeight > max) max=pupilHeight
-//         }
-//     }
-// }
-// ----2
-// const max = Math.max(...schoolsData.flat(Infinity))
-// ---3
-
-// for (let schooll = 0; schooll < schoolsData.length; schooll++) {
-//     for (let classList = 0; classList < schoolsData[schooll].length; classList++) {
-//         for (let pupilIndex = 0; pupilIndex < schoolsData[schooll][classList].length; pupilIndex++) {
-//             const pupilHeight = schoolsData[schooll][classList][pupilIndex]
-//             if(pupilHeight>max) max = pupilHeight
-//         }
-//     }
-// }
-// document.write(max)
-
-const carSalesData = [
-    [
-      // Перша країна
-      [
-        // Перша область
-        [100, 150, 200], // Перше місто, продажі по трьох моделях
-        [120, 180, 220], // Друге місто, продажі по трьох моделях
-        [110, 140, 230], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Друга область
-        [130, 160, 210], // Перше місто, продажі по трьох моделях
-        [140, 170, 240], // Друге місто, продажі по трьох моделях
-        [150, 180, 250], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Третя область
-        [160, 190, 200], // Перше місто, продажі по трьох моделях
-        [170, 180, 230], // Друге місто, продажі по трьох моделях
-        [180, 200, 210], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Четверта область
-        [190, 210, 240], // Перше місто, продажі по трьох моделях
-        [200, 220, 260], // Друге місто, продажі по трьох моделях
-        [210, 230, 250], // Третє місто, продажі по трьох моделях
-        [220, 240, 270], // Четверте місто, продажі по трьох моделях
-      ],
-    ],
-    [
-      // Друга країна
-      [
-        // Перша область
-        [120, 130, 150], // Перше місто, продажі по трьох моделях
-        [130, 140, 160], // Друге місто, продажі по трьох моделях
-        [140, 150, 170], // Третє місто, продажі по трьох моделях
-        [150, 160, 180], // Четверте місто, продажі по трьох моделях
-      ],
-      [
-        // Друга область
-        [160, 170, 180], // Перше місто, продажі по трьох моделях
-        [170, 180, 190], // Друге місто, продажі по трьох моделях
-        [180, 190, 200], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Третя область
-        [190, 200, 210], // Перше місто, продажі по трьох моделях
-        [200, 210, 220], // Друге місто, продажі по трьох моделях
-        [210, 220, 230], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Четверта область
-        [220, 230, 240], // Перше місто, продажі по трьох моделях
-        [230, 240, 250], // Друге місто, продажі по трьох моделях
-        [240, 250, 260], // Третє місто, продажі по трьох моделях
-        [250, 260, 270], // Четверте місто, продажі по трьох моделях
-      ],
-    ],
-    [
-      // Третя країна
-      [
-        // Перша область
-        [130, 140, 150], // Перше місто, продажі по трьох моделях
-        [140, 150, 160], // Друге місто, продажі по трьох моделях
-        [150, 160, 170], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Друга область
-        [160, 170, 180], // Перше місто, продажі по трьох моделях
-        [170, 180, 190], // Друге місто, продажі по трьох моделях
-        [180, 190, 200], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Третя область
-        [190, 200, 210], // Перше місто, продажі по трьох моделях
-        [200, 210, 220], // Друге місто, продажі по трьох моделях
-        [210, 220, 230], // Третє місто, продажі по трьох моделях
-      ],
-      [
-        // Четверта область
-        [220, 230, 240], // Перше місто, продажі по трьох моделях
-        [230, 240, 250], // Друге місто, продажі по трьох моделях
-        [240, 250, 260], // Третє місто, продажі по трьох моделях
-        [250, 260, 270], // Четверте місто, продажі по трьох моделях
-      ],
-    ],
-  ]
-
-// const sum = carSalesData.flat(Infinity).reduce((prevSum, el) => prevSum + el)
-// document.write(sum)
-
-
-// ---1
-// for (let modelIndex = 0; modelIndex <3; modelIndex++) {
-//     let sum = 0
-//     for(let country of carSalesData) {
-//         for (let region of country) {
-//             for (let city of region) {
-//                 sum += city[modelIndex]
-//             }
-//         }
-//     }
-//     document.write(`${sum} <br>`)
-// }
-
-// ---2
-// const modelSum = [0, 0, 0]
-// for (let country of carSalesData) {
-//     for( let region of country) {
-//         for( let city of region ) {
-//             for (let modelIndex = 0; modelIndex <city.length; modelIndex++) {
-//                 modelSum[modelIndex] +=city[modelIndex]
-//             }
-//         }
-//     }
-// }
-// document.write(`${modelSum} <br>`)
-
-// for (let country of carSalesData) {
-//     const sum = country.flat(Infinity).reduce((prevSum, el) => prevSum +el)
-//     document.write(`${sum} <br>`)
-// }
-for (let country of carSalesData) {
-    for (let region of country) {
-        for (let city of region) {
-            const sum = city .flat(Infinity).reduce((prevSum, el) => prevSum +el)
-            document.write(`${sum} <br>`)
-        }
+//номери рядків від 0 до половини, стовпці від 0 до половини
+let sum01 = 0
+for (let row = 0; row < middleRow; row++) {
+    for (let column = 0; column < Math.floor(arr[row].length/2); column++) {
+        sum01 += arr[row][column]
     }
 }
+document.write(`Сума номерів рядків від 0 до половини, стовпці від 0 до половини :${sum01} <br>`)
+
+// номери рядків від 0 до половини, стовпці від половини до кінця
+let sum02 = 0
+for (let row = 0; row < middleRow ; row++) {
+    for (let column = Math.floor(arr[row].length/2); column < arr[row].length; column++) {
+        sum02 += arr[row][column]
+    }
+}
+
+document.write(`Сума номерів рядків від 0 до половини, стовпці від половини до кінця: ${sum02} <br>`)
+
+// номери рядків (від половини до кінця, стовпці від 0 до половини)
+let sum03 = 0
+for (let row = middleRow; row < rowLength; row++) {
+    for (let column = 0; column < Math.floor(arr[row].length/2); column++) {
+        sum03 += arr[row][column]
+    }
+}
+
+document.write(`Сума номерів рядків (від половини до кінця, стовпці від 0 до половини): ${sum03} <br>`)
+
+
+// номери рядків від половини до кінця , стовпці від половини до кінця
+let sum04 = 0
+for (let row = middleRow; row < rowLength; row++) {
+    for (let column = Math.floor(arr[row].length/2); column < arr[row].length; column++) {
+        sum04 += arr[row][column]
+    }
+}
+
+document.write(`Сума номерів рядків від половини до кінця , стовпці від половини до кінця: ${sum04} <br>`)
+
+
+// Суму парних рядків
+let sum05 = 0
+for (let row = 0; row < rowLength; row+=2) {
+    for (let column = 0; column < arr[row].length; column++) {
+        sum05 += arr[row][column]
+    }
+}
+
+document.write(`Сума парних рядків: ${sum05} <br>`)
+
+// Суму непарних стовпців
+let sum06 = 0
+for (let row = 0; row < rowLength; row++) {
+    for (let column = 1; column < arr[row].length; column+=2) {
+        sum06 += arr[row][column]
+    }
+}
+
+document.write(`Сума непарних стовпців: ${sum06} <br>`)
+
+
+// У парних рядках – непарні стовпці, у непарних – парні.
+let sum07 = 0
+for (let row = 0; row < rowLength; row++) {
+    if (row % 2 ===0) {
+        for (let column = 1; column < arr[row].length; column+=2) {
+            sum07 += arr[row][column]
+        }
+    } else {
+        for (let column = 0; column < arr[row].length; column+=2) {
+            sum07 += arr[row][column]
+        }
+    }
+    
+}
+
+document.write(`У парних рядках – непарні стовпці, у непарних – парні: ${sum07} <br>`)
+
--- hw-11/js/task02.js
+++ hw-11/js/task02.js
@@ -1,21 +1,81 @@
 "use strict"
-// Задача. Дано історію цін на цінні папери за деякий період (згенерувати від 1 до 10000).
-// Сформувати новий масив, у якому є номери тільки тих, що більші за 1000 грн.
 
-if (confirm('Почати тестування?')) {
-    const userNum = parseInt(prompt("Введіть кількість елементів в масиві"))
-    
-    function getPricesArr(userNum) {
-        const prices = []
-        for (let i = 0; i < userNum; i++) {
-            let randomPrice = 1 + Math.floor(Math.random() * 10000)
-            prices.push(randomPrice)
-        }
-        return prices
+const profit = [
+    [100, 200, 150, 130, 170, 180, 220], // Магазин 1
+    [90, 210, 130, 140, 160, 190, 200],  // Магазин 2
+    [120, 250, 140, 110, 180, 230, 250]  // Магазин 3
+];
+
+// загальний прибуток кожного масиву за тиждень
+for (let shop of profit ) {
+    let sum01 = 0
+    for (let dayProfit of shop) {
+        sum01 += dayProfit
     }
-    const prices = getPricesArr(userNum) 
-
-    const indexesGreater1000 = prices.map((el, index) => (el > 1000 ? index : -1)).filter(index => index !== -1)
-
-    document.write(`Масив, у якому є номери тільки тих, що більші за 1000 грн. : ${indexesGreater1000}`)
+    document.write(`Загальний прибуток: ${sum01} грн. <br>`)
 }
+// загальний прибуток усіх магазинів по дням (загальний прибуток усіх магазинів за понеділок, за вівторок, і т.д.)
+for (let dayProfitIndex = 0; dayProfitIndex < 7; dayProfitIndex++) {
+    let sum = 0
+    for (let shop of profit) {
+        sum += shop[dayProfitIndex]
+    }
+    document.write(`Прибуток по ${dayProfitIndex+1} дню: ${sum} грн.<br>`)
+}
+// загальний прибуток за робочі дні
+let sum02 = 0
+for (let shopIndex = 0; shopIndex < profit.length; shopIndex++) {
+    for (let dayProfitIndex = 0; dayProfitIndex < 5; dayProfitIndex++) {
+        sum02 += profit[shopIndex][dayProfitIndex]
+    }
+}
+document.write(`Загальний прибуток за робочі дні: ${sum02} грн. <br>`)
+
+// загальний прибуток за вихідні дні
+let sum03 = 0
+for (let shopIndex = 0; shopIndex < profit.length; shopIndex++) {
+    for (let dayProfitIndex = 5; dayProfitIndex < 7; dayProfitIndex++) {
+        sum03 += profit[shopIndex][dayProfitIndex]
+    }
+}
+document.write(`Загальний прибуток за вихідні дні: ${sum03} грн. <br>`)
+
+// максимальний прибуток за середу
+let max = -Infinity
+for (let shopIndex = 0; shopIndex < profit.length; shopIndex++) {
+    if (profit[shopIndex][2] > max) max = profit[shopIndex][2]
+}
+document.write(`Максимальний прибуток за середу: ${max} грн.<br>`)
+
+// сформувати загальний список (одновимірний масив) зі значенням, які що більші за 200
+const newProfit = profit.flat(Infinity).filter(el => el>200)
+document.write(`Загальний список (одновимірний масив) зі значенням, які що більші за 200: ${newProfit} <br>`)
+
+// відсортувати кожен тиждень за зростанням
+for (let shop of profit) {
+    shop.sort((a, b) => a - b)
+    document.write(`Відсортований прибуток: ${shop} <br>`)
+}
+
+// відсортувати тижні (рядки) за спаданням максимального елементи у цьому тижні (рядку), 
+// тобто при порівнянні рядків потрібно порівнювати максимальні елементи у кожному з цих рядків
+
+profit.sort((week1, week2) => {
+    const max1 = Math.max(...week1)
+    const max2 = Math.max(...week2)
+    return max2 - max1
+})
+
+document.write(`Відсортовані тижні за спаданням максимального елемента: ${JSON.stringify(profit)} <br>`)
+
+
+// упорядкувати тижні (рядки) за спаданням суми елементів у рядку (тобто при порівнянні двох рядків треба знайти суму
+// кожного з рядків і порівнювати ці суми, на основі цих сум буде зрозуміло, який з елементів повинен іти раніше)
+
+profit.sort((week1, week2) => {
+    const sum1 = week1.reduce((prevSum, el) => prevSum +el, 0) 
+    const sum2 = week2.reduce((prevSum, el) => prevSum +el, 0)
+    return sum2 - sum1
+});
+
+document.write(`Відсортовані тижні за спаданням суми елементів у рядку: ${JSON.stringify(profit)} <br>`);
