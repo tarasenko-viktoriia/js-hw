fd7bf0be26c3f29e465cab27b2306d0b8abe1634
victoriataracenko <victoriataracenko0412@gmail.com>
2025-03-30 21:45:19 +0300

    done tasks 1,3,4,5

--- hw-15/js/task01.js
+++ hw-15/js/task01.js
@@ -1,1 +1,26 @@
 "use strict"
+// Створити об’єкт «Тир». У масиві зберігаються 1, якщо у цьому квадраті є заєць і 0 в іншому випадку.
+// Поля(властивості)
+// Масив, у якому зберігається поле з зайцями
+// Методи (дії)
+// Метод пострілу (задається позиція пострілу)
+// Виведення ігрового поля
+if (confirm('Почати тестування?')) {
+   const shootingGallery = {
+      playingField: [0, 0, 0, 1],
+
+      posterior(){
+         const userPosterior = parseInt(prompt(`Введіть позицію пострілу від 1 до ${this.playingField.length}`))
+
+         if(this.playingField[userPosterior - 1] === 1){
+            alert("Ви влучили у зайця!")
+            this.playingField[userPosterior-1] = 0
+         } else alert('Немає зайця в цьому квадраті.')
+      },
+      getPlayingField (){
+         document.write(this.playingField)
+      }
+   }
+   shootingGallery.posterior()
+   shootingGallery.getPlayingField ()
+}
