2dec877281b25517d603229de1a1115ddeb4e4ee
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-05 12:20:26 +0300

    done 4-7 tasks hw 17

--- hw-17/js/task05.js
+++ hw-17/js/task05.js
@@ -1,2 +1,24 @@
 "use strict"
 // Відобразити таблицю 3*4 з випадковими числами (її треба динамічно створити і вставити на сторінку)
+
+function createTable(rowNum, colsNum, minRandNum = 0, maxRandNum = 100) {
+   const tableEl = document.createElement('table')
+   tableEl.style.cssText = "border-collapse: collapse; width: 100%;"
+   for (let rowIndex = 0; rowIndex < rowNum; rowIndex++) {
+      const trEl = document.createElement("tr")
+
+      for (let colIndex = 0; colIndex < colsNum; colIndex++) {
+         const tdEl = document.createElement("td")
+
+         tdEl.innerText = minRandNum + Math.floor(Math.random() * (maxRandNum - minRandNum + 1))
+         tdEl.style.cssText = "border: 1px solid black; padding: 8px; text-align: center;";
+         trEl.append(tdEl)
+      }
+      tableEl.append(trEl)
+   }
+   return tableEl
+}
+
+const table1 = createTable(3, 4)
+console.log(table1)
+document.getElementById("table-container").append(table1)
--- hw-17/js/task07.js
+++ hw-17/js/task07.js
@@ -1,30 +1,20 @@
 "use strict"
-// Дано два об’єкта. Обидва містять масив цілих чисел. При цьому у одному з них є функція знаходження суми, 
-// а у іншому – функція для знаходження добутку тих, які знаходяться між заданими мінімальним і максимальних значенням.
-// Використати обидва методи стосовно обидвох об’єктів (використати call, apply)
-
-const obj1 = {
-   arr: [2, 4, 6, 8],
-
-   getSum(){
-      return this.arr.reduce((prevSum, arr) => prevSum + arr, 0)
+// Подорож складається з 3 етапів. На кожному етапі користувач може вибрати один з видів транспорту (авто, автобус, літак - випадаючий список), харчування (сніданок, обід, вечеря – checkbоx) та одного з 3-х гідів(використати  - radio buttons). Ціни визначте самі. Підрахувати загальну вартість.
+function getTotalPrice() {
+   const selectList = document.querySelectorAll("select")
+   let sumSelectList = 0
+   for (const select of selectList) {
+      sumSelectList += parseFloat(select.value)
    }
-}
 
-const obj2 = {
-   arr: [10, 8, 7, 6],
-
-   getProduct(min, max) {
-      let filteredNumbers = this.arr.filter(num => num >= min && num <= max)
-      if (filteredNumbers.length === 0) {
-         return 0
-      }
-      return filteredNumbers.reduce((product, num) => product * num, 1)
+   const inputsList= document.querySelectorAll("input")
+   let sumInputsList = 0
+   for (const inputEl of inputsList) {
+      if(inputEl.checked) sumInputsList += parseFloat (inputEl.value)
    }
+
+   document.getElementById('result').innerText = sumSelectList  + sumInputsList
 }
-console.log(obj1.getSum())
-console.log(obj2.getProduct(6, 7))
-console.log(obj1.getSum.call(obj2))
-console.log(obj2.getProduct.call(obj1,6, 8))
-console.log(obj1.getSum.apply(obj2))
-console.log(obj2.getProduct.apply(obj1,[6, 8]))
+window.onload = function () {
+   document.querySelector('button').onclick = getTotalPrice
+}
