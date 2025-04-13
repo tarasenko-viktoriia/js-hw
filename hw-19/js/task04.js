619cdcd43e3631d185d1c907e0bfabba176aa838
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-13 15:29:08 +0300

    done task 4,5  hw-19

--- hw-19/js/task04.js
+++ hw-19/js/task04.js
@@ -1,19 +1,48 @@
 "use strict"
-// Наперед задано у скрипті масив зі списком бажань. Після завантаження сторінки випадковим чином вибираються 3 
-// і відображаються у окремих div (їх треба створити і додати на сторінку)
-const wishes = ["Гроші", "Великі гроші", "Дуже великі гроші", "Море грошей", "Купа грошей"]
-function createDiv (wishes, numDiv) {
-   const container = document.getElementById("div-container")
-   let copyWishes = [...wishes]
-   for (let i = 0; i < numDiv; i++) {
-      const div = document.createElement("div")
-      const randIndex = Math.floor(Math.random() * copyWishes.length)
-      div.innerText = copyWishes[randIndex]
 
-      copyWishes.splice(randIndex, 1)
+function createTable(rowNum, colsNum, minRandNum = 0, maxRandNum = 100) {
+   const tableEl = document.createElement('table')
+   tableEl.style.cssText = "border-collapse: collapse; width: 100%;"
+   for (let rowIndex = 0; rowIndex < rowNum; rowIndex++) {
+      const trEl = document.createElement("tr")
 
-      container.append(div)
-      
+      for (let colIndex = 0; colIndex < colsNum; colIndex++) {
+         const tdEl = document.createElement("td")
+
+         tdEl.innerText = minRandNum + Math.floor(Math.random() * (maxRandNum - minRandNum + 1))
+         tdEl.style.cssText = "border: 1px solid black; padding: 8px; text-align: center;";
+         trEl.append(tdEl)
+      }
+      tableEl.append(trEl)
    }
+   return tableEl
 }
-createDiv (wishes, 3)
+
+const table1 = createTable(3, 3)
+const table2 = createTable(3, 3)
+const table3 = createTable(3, 3)
+document.querySelector(".table-01").append(table1)
+document.querySelector(".table-02").append(table2)
+document.querySelector(".table-03").append(table3)
+
+const tableContainer = document.querySelector("#table-container")
+tableContainer.addEventListener('click', function(event){
+   const clickedCell = event.target.closest('td')
+   
+   if (clickedCell) {
+      const table = clickedCell.closest('table')  
+      
+      let clicks = table.getAttribute("data-clicks")
+
+      clicks++
+      
+      table.setAttribute("data-clicks", clicks)
+      
+      const tableWrapper = table.parentElement 
+      const clickCounter = tableWrapper.querySelector(".click-num") 
+      
+      clickCounter.innerText = clicks
+
+      table.classList.toggle('selected-table')
+   }
+})
--- hw-19/js/task05.js
+++ hw-19/js/task05.js
@@ -1,24 +1,9 @@
 "use strict"
-// Відобразити таблицю 3*4 з випадковими числами (її треба динамічно створити і вставити на сторінку)
-
-function createTable(rowNum, colsNum, minRandNum = 0, maxRandNum = 100) {
-   const tableEl = document.createElement('table')
-   tableEl.style.cssText = "border-collapse: collapse; width: 100%;"
-   for (let rowIndex = 0; rowIndex < rowNum; rowIndex++) {
-      const trEl = document.createElement("tr")
-
-      for (let colIndex = 0; colIndex < colsNum; colIndex++) {
-         const tdEl = document.createElement("td")
-
-         tdEl.innerText = minRandNum + Math.floor(Math.random() * (maxRandNum - minRandNum + 1))
-         tdEl.style.cssText = "border: 1px solid black; padding: 8px; text-align: center;";
-         trEl.append(tdEl)
-      }
-      tableEl.append(trEl)
+const cardList = document.querySelector(".card__list")
+cardList.addEventListener('click', function(event){
+   const clickedElement = event.target.closest(".card__item")
+   
+   if (clickedElement) {
+      clickedElement.classList.toggle('selected')
    }
-   return tableEl
-}
-
-const table1 = createTable(3, 4)
-console.log(table1)
-document.getElementById("table-container").append(table1)
+})
