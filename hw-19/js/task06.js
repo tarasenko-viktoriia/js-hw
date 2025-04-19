3fbcc4ce497777468635cce23f1c9beb201f6f81
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-19 19:46:25 +0300

    done task 6

--- hw-19/js/task06.js
+++ hw-19/js/task06.js
@@ -1,46 +1,53 @@
 "use strict"
-// Користувач задає кількість оцінок і натискає на кнопку «get table». В результаті формується таблиця з input, куди користувач вводить оцінки. Після цього натискає на кнопку “get sum” і знаходить середнє значення.
+// Дано список спортсменів. Потрібно сформувати список тих, які будуть брати участь у змаганні. При цьому є два стовпці. В одному відображені всі спортсмени, в іншому – список тих, хто був вибраний. При натисканні на зелену стрілку спортсмен переміщається у список для змагань. При натисканні на червону стрілку спортсмен переміщається у загальний список.
 
-function createTable() {
-   const rowNum = parseInt(document.getElementById("num").value)
-
-   const tableEl = document.createElement('table')
-   for (let rowIndex = 0; rowIndex < 1; rowIndex++) {
-      const trEl = document.createElement("tr")
-      for (let colIndex = 0; colIndex < rowNum; colIndex++) {
-         const tdEl = document.createElement("td")
-         const inputEl = document.createElement("input")
-         inputEl.type = "number"
-         tdEl.append(inputEl)
-         trEl.append(tdEl)
-      }
-      tableEl.append(trEl)
+class athletesManager {
+   constructor(athletesList) {
+      this.allAthletes = athletesList
+      this.selectedAthletes = []
+      this.render()
    }
-   return tableEl
-}
-const btnTable = document.getElementById("getTable");
-
-btnTable.onclick = function () {
-   const tableContainer = document.getElementById("table-container")
-   tableContainer.innerText = ""
-   const table = createTable()
-   tableContainer.append(table)
-
-   const btnSum = document.createElement("button")
-   btnSum.id = "btnSum"
-   btnSum.innerText = "Get Sum"
-   tableContainer.append(btnSum)
-
-   btnSum.onclick = getSum
-}
-function getSum() {
-   const container = document.getElementById('table-container')
-   const inputs = container.querySelectorAll('input')
-   let sum = 0
-   for (let i = 0; i < inputs.length; i++) {
-      sum += parseInt(inputs[i].value)
+   moveToSelected(name) {
+      this.allAthletes = this.allAthletes.filter(a => a !== name)
+      this.selectedAthletes.push(name)
+      this.render()
    }
-   let result = sum / inputs.length
 
-   document.getElementById('result').innerText = result.toFixed(2)
+   moveToAll(name) {
+      this.selectedAthletes = this.selectedAthletes.filter(a => a !== name)
+      this.allAthletes.push(name)
+      this.render()
+   }
+
+   createList(container, title, list, isMain) {
+      container.innerHTML = ''
+      const heading = document.createElement('h2')
+      heading.innerText = title
+      container.appendChild(heading)
+   
+      const ul = document.createElement('ul')
+      list.forEach(name => {
+         const li = document.createElement('li')
+         li.innerText = name
+   
+         const btn = document.createElement('button')
+         btn.innerText = isMain ? '➡️' : '⬅️'
+         btn.style.backgroundColor = isMain ? 'lightgreen' : 'lightcoral'
+         btn.onclick = () => isMain ? this.moveToSelected(name) : this.moveToAll(name)
+   
+         li.appendChild(btn)
+         ul.appendChild(li)
+      })
+   
+      container.appendChild(ul)
+   }
+   render() {
+      const generalContainer = document.getElementById('all-athletes')
+      const chosenContainer = document.getElementById('selected-athletes')
+      this.createList(generalContainer, 'Загальний список', this.allAthletes, true)
+      this.createList(chosenContainer, 'Обрані спортсмени', this.selectedAthletes, false)
+   }
 }
+
+const athletesList = ['John Depp', 'Sara Wik', 'Den Miro', 'Alan Woo', 'Olga Sich', 'Ivan Hal']
+const manager = new athletesManager(athletesList)
