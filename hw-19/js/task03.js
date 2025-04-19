72c0c09bc71612be89fcb14a68b5ff0573638a07
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-19 14:34:29 +0300

    done task 3

--- hw-19/js/task03.js
+++ hw-19/js/task03.js
@@ -1,14 +1,32 @@
 "use strict"
-// Користувач задає рік народження. Визначити кількість років користувача.
+// Дано 5 випадковим чином згенерованих нумерованих списків з рандомними числами (кількість елементів у списку випадкова від 1 до 10, елементи випадкові – від 1 до 100). При натисненні на кнопку нумеровані списки з парною кількістю елементів зафарбувати у зелений колір, інші у червоний.
 
-function birthYear (){
-   const birthYear = parseInt(document.getElementById("birthYear").value)
-   const currentYear = new Date().getFullYear()
-   const numYears = currentYear - birthYear
-
-   document.getElementById("result").innerText = numYears
+function createList(num) {
+   const listContainer = document.querySelector(".list-container")
+   for (let i = 1; i <= num; i++) {
+      const olElement = document.createElement("ol")
+      let numOlElements = 1 + Math.floor(Math.random() * 10)
+      for (let j = 1; j <= numOlElements; j++) {
+         const liElement = document.createElement("li")
+         let randNum = 1 + Math.floor(Math.random() * 100)
+         liElement.innerText = randNum
+         olElement.append(liElement)
+      }
+      listContainer.append(olElement)
+   }
 }
 
-window.onload = function () {
-   document.querySelector('button').onclick = birthYear
+
+function colorList() {
+   const olList = document.querySelectorAll('OL')
+   olList.forEach(list => {
+      if(list.children.length % 2 === 0) list.style.color = "green"
+      else list.style.color = "red"
+   })
 }
+
+createList(5)
+
+document.querySelector('button').addEventListener("click", colorList)
+
+
