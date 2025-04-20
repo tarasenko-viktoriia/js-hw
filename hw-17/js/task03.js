230be8c39cac80f242b3e3c6cd495fd47b74d2e7
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-20 13:26:27 +0300

    done task 3

--- hw-17/js/task03.js
+++ hw-17/js/task03.js
@@ -1,14 +1,36 @@
 "use strict"
-// Користувач задає рік народження. Визначити кількість років користувача.
+// Створити клас Нагадувач. Кожні вказані кількості секунд (використати setInterval) програма нагадує про якусь подію (це просто текст) і також виводиться порядковий номер скільки раз вже нагадування було. Зробити так, щоб неможна було зробити одночасно декілька об’єктів-нагадувачів. Методи зупинки таймера, метод зміни повідомлення без зупинки таймера.
 
-function birthYear (){
-   const birthYear = parseInt(document.getElementById("birthYear").value)
-   const currentYear = new Date().getFullYear()
-   const numYears = currentYear - birthYear
+class Reminder {
+   static reminderOfEvent
 
-   document.getElementById("result").innerText = numYears
+   constructor (event) {
+      if(Reminder.reminderOfEvent) return Reminder.reminderOfEvent
+      this.event = event
+      this.count = 0
+      this.timerId = null
+      Reminder.reminderOfEvent = this
+   }
+   toString (){
+      return `${this.count} - ${this.event}<br>`
+   }
+   getEvent(){
+      this.timerId = setInterval(() => {
+         this.count++
+         document.write(this.toString())
+         
+      }, 5000);
+   }
+   stopTimer(){
+      if (this.timerId) {
+         clearInterval(this.timerId);
+         this.timerId = null;
+      }
+   }
+   changeEvent(newEvent) {
+      this.event = newEvent
+   }
 }
 
-window.onload = function () {
-   document.querySelector('button').onclick = birthYear
-}
+const reminder1 = new Reminder("Поїсти")
+reminder1.getEvent()
