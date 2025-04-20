5968c8212391b36fc3b0af75f7ff8cad85dec9e4
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-20 14:14:34 +0300

    done task 4 hw-17

--- hw-17/js/task04.js
+++ hw-17/js/task04.js
@@ -1,19 +1,76 @@
 "use strict"
-// Наперед задано у скрипті масив зі списком бажань. Після завантаження сторінки випадковим чином вибираються 3 
-// і відображаються у окремих div (їх треба створити і додати на сторінку)
-const wishes = ["Гроші", "Великі гроші", "Дуже великі гроші", "Море грошей", "Купа грошей"]
-function createDiv (wishes, numDiv) {
-   const container = document.getElementById("div-container")
-   let copyWishes = [...wishes]
-   for (let i = 0; i < numDiv; i++) {
-      const div = document.createElement("div")
-      const randIndex = Math.floor(Math.random() * copyWishes.length)
-      div.innerText = copyWishes[randIndex]
+// Склад. База товарів, які зберігаються на складі: назва товару, одиниця виміру, кількість, фірма виробник (назва, реєстраційний номер). Організувати реєстрацію/відвантаження товарів, фільтрація за назвою товару, фільтрація за назвою фірми
+class Firm {
+   constructor(name, registrationNumber){
+      this.name = name
+      this.registrationNumber = registrationNumber
+   }
 
-      copyWishes.splice(randIndex, 1)
-
-      container.append(div)
-      
+   toString(){
+      return `Назва фірми виробника: ${this.name}, реєстраційний номер фірми виробника: ${this.registrationNumber}`
    }
 }
-createDiv (wishes, 3)
+
+class Product {
+   constructor(name, unit, quantity, firmName, firmRegNumber) {
+      this.name = name
+      this.unit = unit
+      this.quantity = quantity
+      this.firm = new Firm(firmName, firmRegNumber)
+   }
+
+   toString(){
+      return `Назва товару: ${this.name}, одиниця виміру: ${this.unit}, кількість: ${this.quantity}, ${this.firm}.<br>`
+   }
+}
+
+class Warehouse {
+   constructor(){
+      this.products = []
+   }
+
+   registerProduct(product){
+      this.products.push(product)
+   }
+
+   shipProduct(productName, amount){
+      const productToShip = this.products.find(product => product.name === productName)
+
+      if (productToShip) {
+         if (productToShip.quantity >= amount) {
+            productToShip.quantity -= amount
+            document.write(`Відвантаження ${productToShip.name} відбулося у кількості ${amount} ${productToShip.unit}<br>`)
+         } else {
+            document.write(`Відвантаження ${productToShip.name} не відбулося. На складі недостатня кількість.<br>`)
+         }
+      } else {
+         document.write(`Товар не знайдено.<br>`)
+      }
+   }
+
+   filterByProductName(name) {
+      return this.products.filter(product => product.name === name)
+   }
+
+   filterByFirmName(firmName) {
+      return this.products.filter(product => product.firm.name === firmName)
+   }
+
+   listProducts(){
+      this.products.forEach(product => document.write(product))
+   }
+}
+const firm1 = new Firm("FFF", "12122")
+const firm2 = new Firm("AAA", "1255")
+
+const product1 = new Product("Вода", "літрів", 100, firm1.name, firm1.registrationNumber)
+const product2 = new Product("Сік", "літрів", 50, firm2.name, firm2.registrationNumber)
+
+const warehouse = new Warehouse()
+warehouse.registerProduct(product1)
+warehouse.registerProduct(product2)
+
+warehouse.listProducts()
+
+warehouse.shipProduct("Вода", 30)
+warehouse.shipProduct("Сік", 60)
