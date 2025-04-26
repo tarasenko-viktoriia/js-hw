ca6294c5b15274814b6a67e1ec695e8d8cf8c3c2
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-26 17:00:42 +0300

    done task 4 hw-21

--- hw-21/js/task04.js
+++ hw-21/js/task04.js
@@ -1,48 +1,79 @@
 "use strict"
+// Розробити Класи House, Dog, Bird. Подумайте яким має бути спільний клас предок.
 
-function createTable(rowNum, colsNum, minRandNum = 0, maxRandNum = 100) {
-   const tableEl = document.createElement('table')
-   tableEl.style.cssText = "border-collapse: collapse; width: 100%;"
-   for (let rowIndex = 0; rowIndex < rowNum; rowIndex++) {
-      const trEl = document.createElement("tr")
-
-      for (let colIndex = 0; colIndex < colsNum; colIndex++) {
-         const tdEl = document.createElement("td")
-
-         tdEl.innerText = minRandNum + Math.floor(Math.random() * (maxRandNum - minRandNum + 1))
-         tdEl.style.cssText = "border: 1px solid black; padding: 8px; text-align: center;";
-         trEl.append(tdEl)
-      }
-      tableEl.append(trEl)
+const imagesObj = {
+   house: "../img/house.png",
+   dog: "../img/dog.png",
+   bird: "../img/bird.png",
+}
+class MovableObj {
+   constructor(x, y, imagesObj, interval){
+      this.x = x
+      this.y = y
+      this.imagesObj = imagesObj
+      this.interval = interval
+      this.element = null
    }
-   return tableEl
+
+   render (containerSelector){
+      const img = document.createElement("img")
+      img.src = this.imagesObj
+      img.className = "img"
+      img.style.top = this.y + "px"
+      img.style.left = this.x + "px"
+      img.style.transition = "all 0.5s"
+
+      this.element = img
+
+      if(containerSelector) document.querySelector(containerSelector).append(img)
+   }
+   update(){
+
+   }
+
+   start (){
+      this.timer = setInterval(() => this.update(), this.interval)
+   }
+}
+class House extends MovableObj{
+   constructor(x, y, imagesObj, interval){
+      super(x, y, imagesObj.house, interval)
+      this.growing = true
+   }
+   update(){
+      if(this.growing) this.element.style.transform = "scale(1.2)"
+      else this.element.style.transform = "scale(1)"
+      this.growing = !this.growing
+   }
 }
 
-const table1 = createTable(3, 3)
-const table2 = createTable(3, 3)
-const table3 = createTable(3, 3)
-document.querySelector(".table-01").append(table1)
-document.querySelector(".table-02").append(table2)
-document.querySelector(".table-03").append(table3)
-
-const tableContainer = document.querySelector("#table-container")
-tableContainer.addEventListener('click', function(event){
-   const clickedCell = event.target.closest('td')
-   
-   if (clickedCell) {
-      const table = clickedCell.closest('table')  
-      
-      let clicks = table.getAttribute("data-clicks")
-
-      clicks++
-      
-      table.setAttribute("data-clicks", clicks)
-      
-      const tableWrapper = table.parentElement 
-      const clickCounter = tableWrapper.querySelector(".click-num") 
-      
-      clickCounter.innerText = clicks
-
-      table.classList.toggle('selected-table')
+class Dog extends MovableObj{
+   constructor(x, y, imagesObj, interval){
+      super(x, y, imagesObj.dog, interval)
    }
-})
+   update() {
+      const randX = -500 + Math.floor(Math.random() * 1501)
+      this.element.style.transform = `translateX(${randX}px)`
+   }
+}
+class Bird extends MovableObj{
+   constructor(x, y, imagesObj, interval){
+      super(x, y, imagesObj.bird, interval)
+   }
+   update(){
+      const randX = -500 + Math.floor(Math.random() * 1501);
+      const randY = -300 + Math.floor(Math.random() * 601);
+      this.element.style.transform = `translate(${randX}px, ${randY}px`
+   }
+}
+const house = new House(100, 100, imagesObj, 2000)
+house.render("body")
+house.start()
+
+const dog = new Dog(300, 300, imagesObj, 1500)
+dog.render("body")
+dog.start()
+
+const bird = new Bird(600, 100,imagesObj, 1000)
+bird.render("body")
+bird.start()
