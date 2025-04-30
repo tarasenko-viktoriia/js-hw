c2865eae0a4723dc386c8ca3fce8713a48a3440d
victoriataracenko <victoriataracenko0412@gmail.com>
2025-04-30 11:15:08 +0300

    done hw-20

--- hw-20/js/task05.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-const cardList = document.querySelector(".card__list")
-cardList.addEventListener('click', function(event){
-   const clickedElement = event.target.closest(".card__item")
-   
-   if (clickedElement) {
-      clickedElement.classList.toggle('selected')
-   }
-})
