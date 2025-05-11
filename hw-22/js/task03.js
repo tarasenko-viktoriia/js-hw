3f6dc3edf749bc8c16a77e9f8c4512b9a549ff84
victoriataracenko <victoriataracenko0412@gmail.com>
2025-05-11 22:51:00 +0300

    done task 3 hw 22

--- hw-22/js/task03.js
+++ hw-22/js/task03.js
@@ -1,3 +1,27 @@
 "use strict"
 
 // Зберігати у пам’яті список справ, які користувачу треба виконати (зберігати у localStorage). Періодично випадковим чином вибирати якусь з справ і виводити користувачу (з використанням confirm). Якщо користувач натискає на «Ок», то видаляти цю задачу.
+
+let list = JSON.parse(localStorage.getItem('tasksList')) || ["Помити посуд", "Випити таблетки", "Позайматися спортом", "Поїсти"]
+
+function showReminder() {
+   if (list.length === 0) {
+      return
+   }
+   setTimeout(() => {
+      let randNumReminder = Math.floor(Math.random() * list.length)
+      const reminder = confirm(list[randNumReminder])
+
+      if (reminder) {
+         list.splice(randNumReminder, 1)
+
+         localStorage.setItem('tasksList', JSON.stringify(list))
+      }
+
+      if (list.length > 0) {
+         showReminder()
+      } 
+   }, 1000)
+}
+
+showReminder()
