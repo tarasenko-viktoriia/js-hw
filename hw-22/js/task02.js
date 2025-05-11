5a1d79663e3ad2ae4727474255ef01311d054453
victoriataracenko <victoriataracenko0412@gmail.com>
2025-05-11 22:21:44 +0300

    done task 2 hw22

--- hw-22/js/task02.js
+++ hw-22/js/task02.js
@@ -1,59 +1,40 @@
 "use strict"
-// Створити клас Client. На основі цього класу створити клас GoldenClient
+// Користувач може змінювати колір фону, що вибирає користувач з використанням input type="color"
+// Зберігати цей колір і відновлювати при наступному відкритті. Також зберігати і відображати кількість змін протягом одного сеансу.
 
-class Client {
-   constructor(id, fullName, sumMoney) {
-      this.id = id
-      this.fullName = fullName
-      this.sumMoney =sumMoney
-   }
 
-   addMoney (numMoney){
-      if(numMoney>0) return this.sumMoney +=numMoney
-   }
-   withdrawal (numMoney) {
-      if (numMoney <= this.sumMoney) return this.sumMoney-=numMoney
-      else throw new Error("На рахунку недостатньо коштів");
-   }
+const inputColor = document.getElementById("inputColor")
+const counter = document.getElementById("counter")
 
-   toString (){
-      return `id: ${this.id}, ПІБ: ${this.fullName}, кількість грошей на рахунку: ${this.sumMoney}`
+function initBgColor() {
+   const savedColor = localStorage.getItem("bgColor")
+   if (savedColor) {
+      document.body.style.backgroundColor = savedColor
+      inputColor.value = savedColor
    }
 }
 
-class GoldenClient extends Client {
-   constructor(id, fullName, sumMoney, limit, percentage) {
-      super(id, fullName, sumMoney)
-      this.limit = limit
-      this.percentage = percentage
-      this.creditMoney = 0
+function changeCounter() {
+   let count = parseInt(sessionStorage.getItem("changeCount"))
+   if (isNaN(count)) {
+      count = 0
+      sessionStorage.setItem("changeCount", String(count))
    }
-
-   withdrawal (numMoney) {
-      if(numMoney > (this.sumMoney+this.limit)) throw new Error("На рахунку недостатньо коштів")
-      else{
-         if (numMoney <= this.sumMoney) {
-            this.sumMoney -= numMoney
-         } else {
-            this.creditMoney = numMoney - this.sumMoney
-            this.sumMoney = 0
-         }
-      }
-      return this.sumMoney
-   }
-
-   getPenalty (){
-      return this.creditMoney * (this.percentage/100)
-   }
-
-   toString() {
-      return `${super.toString()}, Ліміт кредиту: ${this.limit}, Відсоток кредиту: ${this.percentage}%`;
-   }
+   counter.innerText = count
+   return count
 }
 
-const client1 = new Client(1, "Ivanov Ivan Ivanovich", 40000)
-const container = document.querySelector(".container")
-container.innerText = client1
+function colorChange() {
+   const color = inputColor.value
+   document.body.style.backgroundColor = color
+   localStorage.setItem("bgColor", color)
 
+   countChange++
+   sessionStorage.setItem("changeCount", countChange)
+   counter.innerText = countChange
+}
 
+initBgColor()
+let countChange = changeCounter()
 
+inputColor.addEventListener('input', colorChange)
